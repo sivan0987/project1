020db5d79c3e0e7f2ab06b0d3e6dfa32bb398595
sivan0987 <93656677+sivan0987@users.noreply.github.com>
2021-11-28 13:06:31 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,51 +1,29 @@
 #include <stdio.h>
-#include "NumClass.h"
-
-int  main(){
-int x,y;
-
-scanf("%d",&x);
-scanf("%d",&y);
-int min = 0;
-int max = 0;
-max = x;
-min = y;
-
-if(y>x){
-        max = y;
-        min = x;
+#include "my_mat.h"
+int main()
+{
+char a;
+scanf("%c",  &a);
+while (a!='D')
+{
+    if(a=='A'){
+      A();
     }
-printf("The Armstrong numbers are:");
-    for(int i = min; i<=max; i++){
-        if(isArmstrong(i)){
-            printf(" %d",i );
-        }
+    if(a=='B'){
+        B();
     }
-    printf("\n");
+    if(a=='C'){
+        int answer =C();
+        printf("%d \n", answer);
+       
+    }
 
-    printf("The Palindromes are:");
-    for(int i = min; i<=max; i++){
-        if(isPalindrome(i)){
-            printf(" %d",i );
-        }
-    }
-    printf("\n");
+    scanf("%c",  &a);
 
-    printf("The Prime numbers are:");
-    for(int i = min; i<=max; i++){
-        if(isPrime(i)){
-            printf(" %d",i );
-        }
-    }
-    printf("\n");
-    printf("The Strong numbers are:");
-
-    for(int i = min; i<=max; i++){
-        if(isStrong(i)){
-            printf(" %d",i );
-        }
-    }
-    printf("\n");
-   return 0;
-   
 }
+return 0;
+}
+
+
+
+
